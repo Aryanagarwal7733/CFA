c2e532bcbac592a2ffd1443d19f47d54204dba04
Aryan <aryanagarwal610@gmail.com>
2025-05-21 23:08:58 +0530

    Fix dependencies, update next.js and react versions, add next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // Uncomment if you want to disable Turbopack
-  // experimental: {
-  //   turbo: false,
-  // },
-  // Uncomment if CSS optimization causes lightningcss errors
-  // experimental: {
-  //   optimizeCss: false,
-  // }
+  /* config options here */
 };
 
 export default nextConfig;
